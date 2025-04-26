4c475d4cf499c7ea68994850c0c42e209292a0b1
Russ <100997420+sharp-coderr@users.noreply.github.com>
2025-04-26 13:51:26 +0500

    Implement hash table

--- assigments/src/Main.java
+++ assigments/src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        var table = new MyHashTable<Integer,String>();
+        table.put(1,"one");
+        table.put(2,"two");
+        table.put(3,"three");
+        table.put(4,"four");
+        table.put(5,"five");
+        table.put(6,"six");
+        table.put(7,"seven");
+        table.put(8,"eight");
+
+        System.out.println(table.getKey("three"));
+
+        System.out.println(table.contains(12));
+        System.out.println(table.get(6));
+
+
     }
 }
---
+++ assigments/src/MyHashTable.java
@@ -0,0 +1,96 @@
+public class MyHashTable<K,V> {
+    private class HashNode<K,V>
+    {
+        private K key;
+        private V value;
+        private HashNode<K,V> next;
+
+        public HashNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+        @Override
+        public String toString() {
+            return "{" + key + ", " + value + '}';
+        }
+    }
+
+    private HashNode<K,V>[] chainArray;
+    private int M = 11;
+    private int size;
+    public MyHashTable()
+    {
+        chainArray = new HashNode[M];
+        size = 0;
+    }
+    public MyHashTable(int M){
+        this.M = M;
+    }
+    public void put(K key,V value){
+        var index = hash(key);
+        var head = chainArray[index];
+        while(head != null){
+            if(head.key.equals(key)){
+                head.value = value;
+                return;
+            }
+            head = head.next;
+        }
+        var newNode = new HashNode<>(key,value);
+        newNode.next = chainArray[index];
+        chainArray[index] = newNode;
+        size++;
+
+    }
+    public V get(K key){
+        var index = hash(key);
+        var head = chainArray[index];
+        while (head != null){
+            if(head.key.equals(key)){
+                return head.value;
+            }
+            head = head.next;
+        }
+        return null;
+    }
+    public void remove(K key){
+        var index = hash(key);
+        HashNode<K,V> head = chainArray[index];
+        HashNode<K,V> prev = null;
+        while (head != null) {
+            if (head.key.equals(key)) {
+                if (prev != null) {
+                    prev.next = head.next;
+                } else {
+                    chainArray[index] = head.next;
+                }
+                size--;
+                return;
+            }
+            prev = head;
+            head = head.next;
+        }
+    }
+    public boolean contains(K key){
+        int index = hash(key);
+        HashNode<K,V> head = chainArray[index];
+
+        while (head != null) {
+            if (head.key.equals(key)) {
+                return true;
+            }
+            head = head.next;
+        }
+        return false;
+    }
+    public K getKey(V value){
+        for (HashNode<K, V> kvHashNode : chainArray) {
+            if (kvHashNode != null && kvHashNode.value == value)
+                return kvHashNode.key;
+        }
+        return null;
+    }
+    private int hash(K key){
+        return key.hashCode() % M;
+    }
+}
